2bb2d51fc0192160f74cf2b10375f29b94310045
bruce-wayne-at-adobe <wbanks@adobe.com>
2016-10-13 07:39:29 -0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -3,7 +3,7 @@ var jcontent = {"categories":[{"name":"people_","score":0.3984375,"detail":{"cel
 var output = document.getElementById('output');
 
 
-// output.innerHTML = jcontent.tags.name
+output.innerHTML = jcontent.tags[1].name
 // var tags = function() {
 // for (i = 0; i < tags.length; i++) {
 
