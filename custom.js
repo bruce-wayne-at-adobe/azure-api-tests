983fc36b7b67a2d6e26bc39e8e4b72fae2aad02a
bruce-wayne-at-adobe <wbanks@adobe.com>
2016-10-13 11:09:58 -0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
 window.onload = function() {
 var jcontent = {"categories":[{"name":"people_","score":0.3984375,"detail":{"celebrities":[]}},{"name":"people_group","score":0.4140625}],"tags":[{"name":"person","confidence":0.999476969242096},{"name":"people","confidence":0.81858372688293457},{"name":"crowd","confidence":0.0085046868771314621}],"requestId":"92d4fbc5-62b0-4857-9a46-43db69a89301","metadata":{"width":1280,"height":960,"format":"Jpeg"}}
-var output = document.getElementById('output');
+var text = document.getElementById('output');
 
 var tags = function() {
  var confidencejson = jcontent.tags
@@ -28,7 +28,7 @@ var i;
 for (i = 0; i < tags.length; i++) {
     text += tags[i] + "<br>";
 }
-document.getElementById("demo").innerHTML = text;
+// document.getElementById("").innerHTML = text;
 
 // var jcontent = [
 //   { 
