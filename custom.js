9b94b0d29434c594804806d52989485218ccfaef
bruce-wayne-at-adobe <wbanks@adobe.com>
2016-10-13 16:48:56 -0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -3,7 +3,7 @@ var jcontent = {"categories":[{"name":"people_","score":0.3984375,"detail":{"cel
 var text2 = document.getElementById('output2');
 
 var jcontent2 = {"categories":[{"name":"others_","score":0.00390625},{"name":"people_","score":0.3125,"detail":{"celebrities":[]}}],"tags":[{"name":"person","confidence":0.99692058563232422},{"name":"spectacles","confidence":0.15585459768772125}],"requestId":"41ff5c28-c766-41fd-b1f9-d1c1d6b047d2","metadata":{"width":1818,"height":1228,"format":"Jpeg"}}
-var text = document.getElementById('output2'); 
+var text = document.getElementById('output'); 
 
  var tags = function() {
  var confidencejson = jcontent.tags.name
@@ -25,8 +25,8 @@ var text = document.getElementById('output2');
  
   for (i = 0; i < 3; i++) {
       var b = "name: " + (jcontent2.tags)[i].name + ", confidence: " + (jcontent2.tags)[i].confidence
-    text.innerHTML = text2.innerHTML + b;
-    text.appendChild(b);
+    text.innerHTML = text2.innerHTML + a;
+    text.appendChild(a);
   }
  //text.innerHTML = jcontent.tags[0].confidence
 };
