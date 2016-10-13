8d9869eac3fa491268f6fdf6ecb04620bf8d5ff3
bruce-wayne-at-adobe <wbanks@adobe.com>
2016-10-13 15:35:46 -0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,6 +1,7 @@
 window.onload = function() {
 var jcontent = {"categories":[{"name":"people_","score":0.3984375,"detail":{"celebrities":[]}},{"name":"people_group","score":0.4140625}],"tags":[{"name":"person","confidence":0.999476969242096},{"name":"people","confidence":0.81858372688293457},{"name":"crowd","confidence":0.0085046868771314621}],"requestId":"92d4fbc5-62b0-4857-9a46-43db69a89301","metadata":{"width":1280,"height":960,"format":"Jpeg"}}
 var text = document.getElementById('output');
+
 var jcontent2 = {"categories":[{"name":"others_","score":0.00390625},{"name":"people_","score":0.3125,"detail":{"celebrities":[]}}],"tags":[{"name":"person","confidence":0.99692058563232422},{"name":"spectacles","confidence":0.15585459768772125}],"requestId":"41ff5c28-c766-41fd-b1f9-d1c1d6b047d2","metadata":{"width":1818,"height":1228,"format":"Jpeg"}}
 var text = document.getElementById('output2'); 
 
@@ -41,7 +42,7 @@ console.log(jcontent.tags);
     text.appendChild(a);
   }
  
-  for (i = 0; i < 3; i++) {
+  for (i = 0; i < 4; i++) {
       var b = "name: " + (jcontent2.tags)[i].name + ", confidence: " + (jcontent2.tags)[i].confidence
     text.innerHTML = text.innerHTML + b;
     text.appendChild(b);
