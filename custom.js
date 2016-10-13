0a5a38134d7530aa43829cfadba68adecedecb41
bruce-wayne-at-adobe <wbanks@adobe.com>
2016-10-13 11:48:01 -0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -9,7 +9,7 @@ var tags = function() {
  console.log(confidencejson);
  console.log(namejson);
 
-console.log(jcontent);
+console.log(jcontent.tags);
 // tags.forEach( function (confidence)
 
 // var jcontent.tags = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
