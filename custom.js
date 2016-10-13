115b328888ed7340f5d619be9d7fed28a04ec5ad
bruce-wayne-at-adobe <wbanks@adobe.com>
2016-10-13 07:49:44 -0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -2,10 +2,12 @@ window.onload = function() {
 var jcontent = {"categories":[{"name":"people_","score":0.3984375,"detail":{"celebrities":[]}},{"name":"people_group","score":0.4140625}],"tags":[{"name":"person","confidence":0.999476969242096},{"name":"people","confidence":0.81858372688293457},{"name":"crowd","confidence":0.0085046868771314621}],"requestId":"92d4fbc5-62b0-4857-9a46-43db69a89301","metadata":{"width":1280,"height":960,"format":"Jpeg"}}
 var output = document.getElementById('output');
 
+var tags = function() {
+var tagjson = jcontent.tags
+console.log(tagjson);
+ output.innerHTML = jcontent.tags[i].each
 
-output.innerHTML = jcontent.tags[i].each
-// var tags = function() {
-// for (i = 0; i < tags.length; i++) {
+ for (i = 0; i < tags.length; i++) {
 
 
  output.innerHTML = jcontent
