7f70aabb708d9c751a8fba21f86e03e151fe0077
bruce-wayne-at-adobe <wbanks@adobe.com>
2016-10-14 15:00:40 -0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -12,7 +12,16 @@ var text3 = document.getElementById('output3');
 var jcontent4 = {"categories":[{"name":"people_","score":0.94921875,"detail":{"celebrities":[]}}],"tags":[{"name":"person","confidence":0.99035865068435669},{"name":"outdoor","confidence":0.99035370349884033},{"name":"woman","confidence":0.902657687664032},{"name":"street","confidence":0.75811612606048584}],"requestId":"abe0e56d-8879-441b-b076-ba1a8da1a092","metadata":{"width":960,"height":1280,"format":"Jpeg"}}
 var text4 = document.getElementById('output4');
 
- 
+var jcontent5 = {"categories":[{"name":"people_","score":0.33203125,"detail":{"celebrities":[]}},{"name":"people_group","score":0.640625}],"tags":[{"name":"person","confidence":0.99981540441513062},{"name":"clothing","confidence":0.93182039260864258},{"name":"posing","confidence":0.92629814147949219},{"name":"suit","confidence":0.69583016633987427}],"requestId":"ed7845f5-e6ac-46ce-bb1f-69f18ce58f0a","metadata":{"width":1280,"height":960,"format":"Jpeg"}}
+var text5 = document.getElementById('output5');
+    
+var jcontent6 = {"categories":[{"name":"people_","score":0.3984375,"detail":{"celebrities":[]}},{"name":"people_group","score":0.44921875}],"tags":[{"name":"person","confidence":0.9997829794883728},{"name":"clothing","confidence":0.95785820484161377},{"name":"posing","confidence":0.83074581623077393},{"name":"suit","confidence":0.63975614309310913}],"requestId":"faf12b74-56da-4921-a86b-3e36abd74070","metadata":{"width":1280,"height":960,"format":"Jpeg"}}
+var text6 = document.getElementById('output6');
+
+var jcontent7 = {"categories":[{"name":"people_group","score":0.6328125,"detail":{"celebrities":[]}}],"tags":[{"name":"person","confidence":0.99835050106048584},{"name":"indoor","confidence":0.93220812082290649},{"name":"posing","confidence":0.826675534248352}],"requestId":"75616523-ca5e-47b3-9db4-5b2361138f15","metadata":{"width":2576,"height":1952,"format":"Jpeg"}}
+var text7 = document.getElementById('output7');
+    
+    
  for (i = 0; i < 3 ; i++) {
       var a = "name: " + (jcontent.tags)[i].name + ", confidence: " + (jcontent.tags)[i].confidence
     text.innerHTML = text.innerHTML + "<br>" + a;
@@ -25,17 +34,12 @@ text.innerHTML = text.innerHTML + 'jcontent';
   }
  console.log(jcontent)
   for (i = 0; i < 2; i++) {
-//       debugger;
       var b = "name: " + (jcontent2.tags)[i].name + ", confidence: " + (jcontent2.tags)[i].confidence
-         text2.innerHTML = text2.innerHTML + "</br>" + b;
-      
-   var output2 = document.getElementById('output2')
- 
+         text2.innerHTML = text2.innerHTML + "</br>" + b;   
+   var output2 = document.getElementById('output2') 
    text2.innerHTML = text2.innerHTML + 'jcontent2';
-//     text2.appendChild(b);
   }
  console.log(jcontent2)
-//  console.log(jcontent3)
    for (i = 0; i < 1; i++) {
       var c = "name: " + (jcontent3.tags)[i].name + ", confidence: " + (jcontent3.tags)[i].confidence
          text3.innerHTML = text3.innerHTML + "</br>" + c;
@@ -44,20 +48,17 @@ text.innerHTML = text.innerHTML + 'jcontent';
  
    text3.innerHTML = text3.innerHTML + 'jcontent3';
        console.log(jcontent3);
-//     text2.appendChild(b);
   }
     
-   
-//  console.log(jcontent3)
    for (i = 0; i < 4; i++) {
       var d = "name: " + (jcontent4.tags)[i].name + ", confidence: " + (jcontent4.tags)[i].confidence
          text4.innerHTML = text4.innerHTML + "</br>" + d;
         console.log(jcontent4)   
-   //var output4 = document.getElementById('output4')
- 
-   //text4.innerHTML = text4.innerHTML + 'jcontent4';
-   //    console.log(jcontent4);
-//     text2.appendChild(b);
   }
- //text.innerHTML = jcontent.tags[0].confidence
+      for (i = 0; i < 4; i++) {
+      var e = "name: " + (jcontent5.tags)[i].name + ", confidence: " + (jcontent5.tags)[i].confidence
+         text5.innerHTML = text5.innerHTML + "</br>" + e;
+        console.log(jcontent5)   
+  } 
+
 };
