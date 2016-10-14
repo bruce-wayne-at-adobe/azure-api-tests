dcb67ca1fe6f13b8dff4e75b8bdb10cdc661e00e
bruce-wayne-at-adobe <wbanks@adobe.com>
2016-10-13 17:02:09 -0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,9 +1,10 @@
 window.onload = function() {
+
 var jcontent = {"categories":[{"name":"people_","score":0.3984375,"detail":{"celebrities":[]}},{"name":"people_group","score":0.4140625}],"tags":[{"name":"person","confidence":0.999476969242096},{"name":"people","confidence":0.81858372688293457},{"name":"crowd","confidence":0.0085046868771314621}],"requestId":"92d4fbc5-62b0-4857-9a46-43db69a89301","metadata":{"width":1280,"height":960,"format":"Jpeg"}}
-var text = document.getElementById('output2');
+var text = document.getElementById('output');
 
 var jcontent2 = {"categories":[{"name":"others_","score":0.00390625},{"name":"people_","score":0.3125,"detail":{"celebrities":[]}}],"tags":[{"name":"person","confidence":0.99692058563232422},{"name":"spectacles","confidence":0.15585459768772125}],"requestId":"41ff5c28-c766-41fd-b1f9-d1c1d6b047d2","metadata":{"width":1818,"height":1228,"format":"Jpeg"}}
-var text2 = document.getElementById('output'); 
+var text2 = document.getElementById('output2'); 
 
  var tags = function() {
  var confidencejson = jcontent.tags.name
@@ -12,7 +13,7 @@ var text2 = document.getElementById('output');
  console.log(confidencejson);
  console.log(namejson);
 
- console.log(jcontent.tags);
+ console.log(jcontent);
  console.log("this is the jcontent:"jcontent2);
  }
  
