7ed1446d10027dfd2c6ce24db8b266f58e0e4d30
bruce-wayne-at-adobe <wbanks@adobe.com>
2016-10-14 15:24:55 -0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -72,4 +72,37 @@ text.innerHTML = text.innerHTML + 'jcontent';
         console.log(jcontent7)   
   }
 
+  function sendFileRequest(file) {
+
+        var params = {
+            // Request parameters
+            "visualFeatures": "Categories,Tags",
+            "details": "Celebrities",
+        };
+      
+        $.ajax({
+            url: "https://api.projectoxford.ai/vision/v1.0/analyze?" + $.param(params),
+            beforeSend: function(xhrObj){
+                // Request headers
+                xhrObj.setRequestHeader("Content-Type","application/octet-stream");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","ef16492357394ee2b719b50fb10cd6c9");
+            },
+            type: "POST",
+            // Request body
+            data: file,
+            processData: false,
+            contentType: "application/octet-stream"
+        })
+        .done(function(data) {
+            alert("success");
+        })
+        .fail(function(jqHQR, status) {
+            alert("error : " + jqHQR + " status " + status);
+        });
+
+}
+
+
+console.log("checking in from custom!")
+    
 };
