2d137235c519d098626efcfca3956f5afcd61d7f
bruce-wayne-at-adobe <wbanks@adobe.com>
2016-10-17 14:49:19 -0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,8 +1,4 @@
-window.onload = function() {
-
- 
-	React.render(React.createElement('div', null, 'React is Working!'), document.body);	  
-      
+window.onload = function() {  
     
 var jcontent = {"categories":[{"name":"people_","score":0.3984375,"detail":{"celebrities":[]}},{"name":"people_group","score":0.4140625}],"tags":[{"name":"person","confidence":0.999476969242096},{"name":"people","confidence":0.81858372688293457},{"name":"crowd","confidence":0.0085046868771314621}],"requestId":"92d4fbc5-62b0-4857-9a46-43db69a89301","metadata":{"width":1280,"height":960,"format":"Jpeg"}}
 var text = document.getElementById('output');
